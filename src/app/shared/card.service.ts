00077ab73f6e3187b228fa2105676b85f4ea8f9b
matt-grimsley <rmattgrimsley@gmail.com>
2022-11-03 22:34:35 -0500

    Update card.service.ts

--- src/app/shared/card.service.ts
+++ src/app/shared/card.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Card } from './card';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CardService {
+    cards: Card[] = [];
+    activeCards: Card[] = [];
 
-  constructor() { }
+    constructor() {}
 }
